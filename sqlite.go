05a893e9c3fb9323f0659d23222dc33ca051c0e7
kinggo <30891428+longlihale@users.noreply.github.com>
2021-10-28 13:14:51 +0800

    fixed:lower versions of sqlite do not support returning (#61)
    * fixed:lower versions of sqlite do not support returning
    
    * remove comments
    
    * add default register
    
    * remove c
    
    * add delete returning
    
    * fixed:lower versions of sqlite do not support returning
    
    Co-authored-by: lilong21 <lilong21@jd.com>
    Co-authored-by: 李龙 <lilong.21@bytedance.com>

--- sqlite.go
+++ sqlite.go
@@ -1,13 +1,15 @@
 package sqlite
 
 import (
+	"context"
 	"database/sql"
 	"strconv"
 	"strings"
 
+	"gorm.io/gorm/callbacks"
+
 	_ "github.com/mattn/go-sqlite3"
 	"gorm.io/gorm"
-	"gorm.io/gorm/callbacks"
 	"gorm.io/gorm/clause"
 	"gorm.io/gorm/logger"
 	"gorm.io/gorm/migrator"
@@ -36,21 +38,32 @@ func (dialector Dialector) Initialize(db *gorm.DB) (err error) {
 		dialector.DriverName = DriverName
 	}
 
-	// register callbacks
-	callbacks.RegisterDefaultCallbacks(db, &callbacks.Config{
-		CreateClauses:        []string{"INSERT", "VALUES", "ON CONFLICT", "RETURNING"},
-		UpdateClauses:        []string{"UPDATE", "SET", "WHERE", "RETURNING"},
-		DeleteClauses:        []string{"DELETE", "FROM", "WHERE", "RETURNING"},
-		LastInsertIDReversed: true,
-	})
-
 	if dialector.Conn != nil {
 		db.ConnPool = dialector.Conn
 	} else {
-		db.ConnPool, err = sql.Open(dialector.DriverName, dialector.DSN)
+		conn, err := sql.Open(dialector.DriverName, dialector.DSN)
 		if err != nil {
 			return err
 		}
+		db.ConnPool = conn
+	}
+
+	var version string
+	if err := db.ConnPool.QueryRowContext(context.Background(), "select sqlite_version()").Scan(&version); err != nil {
+		return err
+	}
+	// https://www.sqlite.org/releaselog/3_35_0.html
+	if compareVersion(version, "3.35.0") >= 0 {
+		callbacks.RegisterDefaultCallbacks(db, &callbacks.Config{
+			CreateClauses:        []string{"INSERT", "VALUES", "ON CONFLICT", "RETURNING"},
+			UpdateClauses:        []string{"UPDATE", "SET", "WHERE", "RETURNING"},
+			DeleteClauses:        []string{"DELETE", "FROM", "WHERE", "RETURNING"},
+			LastInsertIDReversed: true,
+		})
+	} else {
+		callbacks.RegisterDefaultCallbacks(db, &callbacks.Config{
+			LastInsertIDReversed: true,
+		})
 	}
 
 	for k, v := range dialector.ClauseBuilders() {
@@ -178,4 +191,28 @@ func (dialectopr Dialector) SavePoint(tx *gorm.DB, name string) error {
 func (dialectopr Dialector) RollbackTo(tx *gorm.DB, name string) error {
 	tx.Exec("ROLLBACK TO SAVEPOINT " + name)
 	return nil
+}
+
+func compareVersion(version1, version2 string) int {
+	n, m := len(version1), len(version2)
+	i, j := 0, 0
+	for i < n || j < m {
+		x := 0
+		for ; i < n && version1[i] != '.'; i++ {
+			x = x*10 + int(version1[i]-'0')
+		}
+		i++
+		y := 0
+		for ; j < m && version2[j] != '.'; j++ {
+			y = y*10 + int(version2[j]-'0')
+		}
+		j++
+		if x > y {
+			return 1
+		}
+		if x < y {
+			return -1
+		}
+	}
+	return 0
 }
