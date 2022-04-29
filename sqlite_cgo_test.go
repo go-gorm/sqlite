92880046f5e60aa6ae66c83cecc21913156cc0f5
Arsen Musayelyan <moussaelianarsen@gmail.com>
2022-04-28 22:23:38 -0700

    Add tests for purego driver

--- sqlite_cgo_test.go
+++ sqlite_cgo_test.go
@@ -1,3 +1,5 @@
+//+build !purego
+
 package sqlite
 
 import (
---
+++ sqlite_purego_test.go
@@ -0,0 +1,103 @@
+//+build purego
+
+package sqlite
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"testing"
+
+	"modernc.org/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestDialector(t *testing.T) {
+	// This is the DSN of the in-memory SQLite database for these tests.
+	const InMemoryDSN = "file:testdatabase?mode=memory&cache=shared"
+
+	sqlite.MustRegisterDeterministicScalarFunction(
+		"my_custom_function",
+		-1,
+		func(ctx *sqlite.FunctionContext, args []driver.Value) (driver.Value, error) {
+			return "my-result", nil
+		},
+		
+	)
+
+	rows := []struct {
+		description  string
+		dialector    *Dialector
+		openSuccess  bool
+		query        string
+		querySuccess bool
+	}{
+		{
+			description: "Default driver",
+			dialector: &Dialector{
+				DSN: InMemoryDSN,
+			},
+			openSuccess:  true,
+			query:        "SELECT 1",
+			querySuccess: true,
+		},
+		{
+			description: "Explicit default driver",
+			dialector: &Dialector{
+				DriverName: DriverName,
+				DSN:        InMemoryDSN,
+			},
+			openSuccess:  true,
+			query:        "SELECT 1",
+			querySuccess: true,
+		},
+		{
+			description: "Bad driver",
+			dialector: &Dialector{
+				DriverName: "not-a-real-driver",
+				DSN:        InMemoryDSN,
+			},
+			openSuccess: false,
+		},
+		{
+			description: "Explicit default driver, custom function",
+			dialector: &Dialector{
+				DriverName: DriverName,
+				DSN:        InMemoryDSN,
+			},
+			openSuccess:  true,
+			query:        "SELECT my_custom_function()",
+			querySuccess: true,
+		},
+	}
+	for rowIndex, row := range rows {
+		t.Run(fmt.Sprintf("%d/%s", rowIndex, row.description), func(t *testing.T) {
+			db, err := gorm.Open(row.dialector, &gorm.Config{})
+			if !row.openSuccess {
+				if err == nil {
+					t.Errorf("Expected Open to fail.")
+				}
+				return
+			}
+
+			if err != nil {
+				t.Errorf("Expected Open to succeed; got error: %v", err)
+			}
+			if db == nil {
+				t.Errorf("Expected db to be non-nil.")
+			}
+			if row.query != "" {
+				err = db.Exec(row.query).Error
+				if !row.querySuccess {
+					if err == nil {
+						t.Errorf("Expected query to fail.")
+					}
+					return
+				}
+
+				if err != nil {
+					t.Errorf("Expected query to succeed; got error: %v", err)
+				}
+			}
+		})
+	}
+}
